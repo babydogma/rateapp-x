1e0f1b00301024c4f7b472f4cd60009133fe3436
babydogma <123qqwes@mail.ru>
2026-03-24 14:37:56 +0300

    Update categories.js

--- categories.js
+++ categories.js
@@ -29,14 +29,6 @@ const DEFAULT_CATEGORIES = [
   { name: "Семья", emoji: "👨‍👩‍👧" }
 ];
 
-const SUPABASE_URL = "https://qlogmylywwdbczxolidl.supabase.co";
-const SUPABASE_KEY = "sb_publishable_nVqkHQmgMKoA_F_ft7yfXQ_OWjYq7f4";
-
-const supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
-
 const modalState = {
   mode: "create",
   editIndex: null,
